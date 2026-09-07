7ade13c4187c23185b31682160a7ef4b15a02a27
ANAHI18-BM <bellamorocho6@gmail.com>
2026-09-06 21:10:46 -0500

    Delete Linda_Fashion_Boutique_Avance12_COMPLETO directory

--- Linda_Fashion_Boutique_Avance12_COMPLETO/forms/__init__.py
+++
@@ -1,6 +0,0 @@
-from .producto_form import ProductoForm
-from .cliente_form import ClienteForm
-from .proveedor_form import ProveedorForm
-from .facturacion_form import FacturacionForm
-
-__all__ = ['ProductoForm', 'ClienteForm', 'ProveedorForm', 'FacturacionForm']
--- Linda_Fashion_Boutique_Avance12_COMPLETO/forms/facturacion_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SelectField, IntegerField, DecimalField
-from wtforms.validators import DataRequired, NumberRange
-
-class FacturacionForm(FlaskForm):
-    cliente_id = SelectField('Cliente', coerce=int, validators=[DataRequired()])
-    producto_id = SelectField('Producto', coerce=int, validators=[DataRequired()])
-    cantidad = IntegerField('Cantidad', validators=[DataRequired(), NumberRange(min=1)])
-    precio_unitario = DecimalField('Precio unitario ($)', places=2, validators=[DataRequired(), NumberRange(min=0.01)])
